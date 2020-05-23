db83557be30d44b5c61dce4af9a72789e5ae0d04
Johann Saratka <johann.saratka@hotmail.com>
2020-05-23 19:21:33 +0200

    Added statement syntax tree

--- jlox/tools/main/GenerateAst.java
+++ jlox/tools/main/GenerateAst.java
@@ -20,6 +20,10 @@ public class GenerateAst {
 				"Literal	: Object value",
 				"Unary		: Token operator, Expr right"
 				));
+		defineAst(outputDir, "Stmt", Arrays.asList(
+				"Expression	: Expr expression",
+				"Print		: Expr expression"
+				));
 	}
 	/**
 	 * Generates abstract syntax tree from list of types
