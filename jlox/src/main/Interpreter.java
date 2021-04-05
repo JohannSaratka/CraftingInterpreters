0e5ab97d6cee70820ffc673c21e9489f0becb8f1
JohannSaratka <johann.saratka@hotmail.com>
2021-04-05 14:50:56 +0200

    Added while loop

--- jlox/src/main/Interpreter.java
+++ jlox/src/main/Interpreter.java
@@ -81,7 +81,16 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 		environment.define(stmt.name.lexeme, value);
 		return null;
 	}
-
+	
+	@Override
+	public Void visitWhileStmt(Stmt.While stmt) {
+		while (isTruthy(evaluate(stmt.condition))) {
+			execute(stmt.body);
+		}
+		
+		return null;
+	}
+	
 	@Override
 	public Object visitAssignExpr(Expr.Assign expr) {
 		Object value = evaluate(expr.value);
--- jlox/src/main/Parser.java
+++ jlox/src/main/Parser.java
@@ -52,7 +52,16 @@ class Parser {
 		consume(TokenType.SEMICOLON, "Expect ';' after variable declaration.");
 		return new Stmt.Var(name, initializer);
 	}
-
+	
+	private Stmt whileStatement() {
+		consume(TokenType.LEFT_PAREN, "Expect '(' after 'while'.");
+		Expr condition = expression();
+		consume(TokenType.RIGHT_PAREN, "Expect ')' after condition.");
+		Stmt body = statement();
+		
+		return new Stmt.While(condition, body);
+	}
+	
 	private Stmt statement() {
 		if (match(TokenType.IF)) {
 			return ifStatement();
@@ -60,7 +69,9 @@ class Parser {
 		if (match(TokenType.PRINT)) {
 			return printStatement();
 		}
-		
+		if (match(TokenType.WHILE)) {
+			return whileStatement();
+		}
 		if (match(TokenType.LEFT_BRACE)) {
 			return new Stmt.Block(block());
 		}
